07e400f0c1640809f059dda1e325213199812ddc
Promise Emmanuel Oluwadare <95595405+PromiseGameFi@users.noreply.github.com>
2026-01-11 20:13:09 +0100

    Create index.ts

---
+++ vibeloop/core/index.ts
@@ -0,0 +1,7 @@
+// Core module exports
+
+export * from './types';
+export { ExitDetector, type ExitDetectorConfig } from './exit-detector';
+export { CircuitBreaker, type CircuitBreakerConfig } from './circuit-breaker';
+export { SessionManager, type SessionManagerConfig } from './session-manager';
+export { LoopEngine, type LoopEngineConfig } from './loop-engine';
